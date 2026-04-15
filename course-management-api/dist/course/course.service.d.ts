4923e20bcf6468129ba700f83fc07a384354cf29
Mursalin_Leon <121357813+Le-ArteX@users.noreply.github.com>
2026-04-15 17:32:05 +0600

    Delete course-management-api directory

--- course-management-api/dist/course/course.service.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class CourseService {
-    getCourse(): string;
-    getCourseById(id: number): string;
-    createCourse(): string;
-    updateCourseById(id: string): string;
-    patchCourseById(id: string): string;
-    deleteCourseById(id: string): string;
-}
